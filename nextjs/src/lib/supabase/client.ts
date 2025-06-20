c70b91da1dee781ebe107bbfe03c2d06890e2f2f
Qiulin Li <liqiulin@outlook.com>
2025-06-20 14:00:48 +0800

    打印 supabase 日志

--- nextjs/src/lib/supabase/client.ts
+++ nextjs/src/lib/supabase/client.ts
@@ -1,12 +1,15 @@
-import {createBrowserClient} from '@supabase/ssr'
-import {ClientType, SassClient} from "@/lib/supabase/unified";
-import {Database} from "@/lib/types";
+import { createBrowserClient } from '@supabase/ssr'
+import { ClientType, SassClient } from "@/lib/supabase/unified";
+import { Database } from "@/lib/types";
 
 export function createSPAClient() {
-    return createBrowserClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+
+    const apiUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+    const apiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+    console.info({ 'api_url': apiUrl, 'api_key': apiKey });
+
+    return createBrowserClient<Database>(apiUrl, apiKey)
 }
 
 export async function createSPASassClient() {
