dd228564ad1cc1fb65b40ba190851998b37a02c1
Qiulin Li <liqiulin@outlook.com>
2025-06-20 12:12:07 +0800

    打印日志

--- nextjs/src/lib/supabase/middleware.ts
+++ nextjs/src/lib/supabase/middleware.ts
@@ -6,9 +6,14 @@ export async function updateSession(request: NextRequest) {
         request,
     })
 
+    const apiUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+    const apiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+    console.info({'api_url': apiUrl, 'api_key': apiKey});
+
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        apiUrl,
+        apiKey,
         {
             cookies: {
                 getAll() {
